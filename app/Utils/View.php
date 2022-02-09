ff87a15346c202d3eadaafb747aa738e07d8fa19
Marcelo Vitor <marcelov050799@gmail.com>
2022-02-09 17:56:53 -0300

    Refactory View Helpers - change require_once to include

--- app/Utils/View.php
+++ app/Utils/View.php
@@ -5,5 +5,5 @@ function view($page, $params = [])
     foreach ($params as $key => $param) {
         $$key = $param;
     }
-    return require_once 'app/Pages/' . $page . '.php';
+    return include 'app/Pages/' . $page . '.php';
 }
