68333eb603b9327f01c35a607ae43b7b59c3a81b
Marcelo Vitor <marcelov050799@gmail.com>
2022-02-14 21:17:00 -0300

    coments archive test.php

--- test.php
+++ test.php
@@ -1,51 +1,50 @@
 <?php
-echo "<pre>";
 
-$routes = [
-    [
-        "name" => 'index',
-        "url" => ''
-    ],
-    [
-        "name" => 'show',
-        "url" => 'show/{id}/cachorro/{dog}'
-    ],
-];
+////////////////////////////////////////////////////////////
+/// REFATORANDO METODO DE ROTA PARA ACFEITAR PARAMETROS ///
+///////////////////////////////////////////////////////////
+// $routes = [
+//     [
+//         "name" => 'index',
+//         "url" => ''
+//     ],
+//     [
+//         "name" => 'show',
+//         "url" => 'show/{id}/cachorro/{dog}'
+//     ],
+// ];
 
-$uri = $_GET['url'] ?? '';
+// $uri = $_GET['url'] ?? '';
 
-$uri = explode('/', $uri);
+// $uri = explode('/', $uri);
 
-$rota = false;
-
-
-foreach ($routes as $route) {
-    $routeUri = explode('/', $route['url']);
-    $params = [];
-
-    $error = false;
-
-    if (count($routeUri) == count($uri)) {
-        foreach ($routeUri as $key => $router) {
-            if (strpos($router, '}')) {
-                $param = explode('}', explode('{', $router)[1])[0];
-                $params[$param] = $uri[$key];
-            } else if ($router != $uri[$key]) {
-                $error = true;
-                break;
-            }
-        }
-    } else {
-        $error = true;
-    }
-
-    if (!$error) {
-        $rota = $route;
-        break;
-    }
-}
-
-var_dump($rota);
+// $rota = false;
 
 
-echo "</pre>";
+// foreach ($routes as $route) {
+//     $routeUri = explode('/', $route['url']);
+//     $params = [];
+
+//     $error = false;
+
+//     if (count($routeUri) == count($uri)) {
+//         foreach ($routeUri as $key => $router) {
+//             if (strpos($router, '}')) {
+//                 $param = explode('}', explode('{', $router)[1])[0];
+//                 $params[$param] = $uri[$key];
+//             } else if ($router != $uri[$key]) {
+//                 $error = true;
+//                 break;
+//             }
+//         }
+//     } else {
+//         $error = true;
+//     }
+
+//     if (!$error) {
+//         $rota = $route;
+//         break;
+//     }
+// }
+// var_dump($rota);
+///////////////////////////////////////////////////////////
